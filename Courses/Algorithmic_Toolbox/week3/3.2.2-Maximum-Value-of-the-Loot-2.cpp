ef79d52996ee3d9092a5b19e58a1b83802ced865
Chandrakanth V <chandrakanthvarakala@gmail.com>
2023-06-11 09:13:28 +0530

    Adding no_finder in week4 of ALgorithmic_Toolbox.

--- Courses/Algorithmic_Toolbox/week3/3.2.2-Maximum-Value-of-the-Loot-2.cpp
+++ Courses/Algorithmic_Toolbox/week3/3.2.2-Maximum-Value-of-the-Loot-2.cpp
@@ -9,37 +9,36 @@ int main()
     int n;
     int W;
     cin >> n >> W;
-    vector<pair<int, int>> weights(n);
+    vector<pair<int, int> > weights(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> weights[i].first >> weights[i].second;
     }
 
     // Sorting based on the ratio of first/second
-    std::sort(weights.begin(), weights.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+    sort(weights.begin(), weights.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
         if (a.first == 0 || b.first == 0) {
             return a.first > b.first; // Move items with weight zero to the end
         }
         return static_cast<double>(a.second) / a.first < static_cast<double>(b.second) / b.first;
     });
 
-    double curr = 0;
+    // for(auto i:weights){
+    //     cout<<i.first<<" "<<i.second<<endl;
+    // }
+    // cout<<"Done Sorting ...."<<endl;
+    double curr_W = 0;
     double val = 0;
     int i = 0;
-    while (W - curr > 0 && weights[i].second<W && i < n) {
-        curr += weights[i].second;
-        val += weights[i].first;
-        ++i;
+    while(i<n && curr_W+weights[i].second<=W){
+        curr_W+=weights[i].second;
+        val+=weights[i].first;
+        i++;
     }
-
-    if (curr != W && i < n) {
-        int remaining = W - curr;
-        if (remaining < weights[i].second) {
-            curr += remaining;
-            val += (static_cast<double>(remaining) * (weights[i].first / static_cast<double>(weights[i].second)));
-
+    if(curr_W!=W){
+        if(W-curr_W<weights[i].second){
+            val+=((W-curr_W)*(static_cast<double>(weights[i].first)/static_cast<double>(weights[i].second)));
         }
     }
-
-    printf("%.4f",val);
+    printf("%.3f",val);
     return 0;
 }
